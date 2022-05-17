ed6a01d24b7ca229637a10f61614d8cb3ed02bc0
MFact <mcfactory88@gmail.com>
2022-05-16 23:12:47 -0500

    js var and console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,24 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates the prompt for robot name
+
+var playerName = window.prompt("What is your robot's name");
+
+console.log(playerName);
+
+// this will do math and log 20
+
+console.log(10 + 10)
+
+//what is this?
+
+console.log("Our robot's name is " + playerName)
+
+var name = "Michael";
+console.log(name);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!")
+}
+
+//fight();
