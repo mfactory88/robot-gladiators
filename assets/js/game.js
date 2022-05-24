719cfa6c4f282ea2bdb56a0ba27548fee64f03b3
MFact <mcfactory88@gmail.com>
2022-05-24 16:16:36 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -15,6 +15,52 @@ for(var i = 0; i < enemyNames.length; i++) {
     console.log(enemyNames[i] + " is at " + i + " index")
 }
 
+// function to start a new game
+var startGame = function() {
+    // reset player stats
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for (var i = 0; i < enemyNames.length; i++) {
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+            var pickedEnemyName = enemyNames[i];
+            enemyHealth = 50;
+            fight(pickedEnemyName);
+        }
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    // after the loop ends, player is either out of health or enemies, run endGame
+    endGame();
+
+    // play again
+    //startGame();
+}
+
+var endGame = function() {
+    // if player is alive, player wins
+    if(playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    // ask player if they want to play again
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!")
+    }
+}
 
 var fight = function(enemyName) {
     // repeat and execute as long as enemy is alive
@@ -71,7 +117,7 @@ var fight = function(enemyName) {
             // check player's health
             if (playerHealth <=0) {
                 window.alert(playerName + " has died!");
-                //break;
+                break;
             }
             else {
                 window.alert(playerName + " still has " + playerHealth + " health left.");
@@ -106,3 +152,6 @@ for(var i = 0; i < enemyNames.length; i++) {
         break;
     }
 }
+
+// start the game when the page loads
+startGame();
